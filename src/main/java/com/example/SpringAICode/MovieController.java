2eba04b4c4b7ed96dd82f9899c7a7e086eb7793b
Rohit <rohit11joshi10@gmail.com>
2026-07-28 23:38:00 +0530

    Structured Output Converter: List Output Converter

---
+++ src/main/java/com/example/SpringAICode/MovieController.java
@@ -0,0 +1,32 @@
+package com.example.SpringAICode;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.stringtemplate.v4.ST;
+
+import java.util.List;
+
+public class MovieController {
+
+    private ChatClient chatClient;
+
+    public MovieController(OllamaChatModel chatModel) {
+        this.chatClient = ChatClient.create(chatModel);
+    }
+
+    @GetMapping("movies")
+    public List<String> getMovies(@RequestParam String name){
+        List<String> movies = chatClient.prompt()
+                .user(u -> u.text("List top 5 movies of {name}").param("name", name))
+                .call()
+                .entity(new ListOutputConverter(new DefaultConversionService()));
+
+        return movies;
+    }
+
+
+}
